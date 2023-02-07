e321d4471ca3732ea2ec6ba9827143b415d2fb1b
Alex Dzyuba <alexeydzyuba27@gmail.com>
2023-02-07 11:04:55 +0100

    csr

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const likesCountElem = document.querySelector('span')
 const likeButton = document.querySelector('button')
 
+// Render current likes count
+fetch('http://localhost:8000/likes')
+  .then(response => response.text())
+  .then(currentLikesCount => {
+    likesCountElem.innerText = currentLikesCount
+  })
+
 // Update likes count on click
 likeButton.addEventListener('click', () => {
   // Обновить количество лайков
@@ -9,7 +16,7 @@ likeButton.addEventListener('click', () => {
   likesCountElem.innerText = likesCount
 
   // Отправить запрос на сервер чтобы сервер у себя обновил количество лайков
-  fetch('http://localhost:8000', {
+  fetch('http://localhost:8000/update-likes', {
     method: 'POST',
     body: likesCount
   })
